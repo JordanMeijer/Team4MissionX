6c36237fe797336884008b484bd1a04741ce319d
Phillip Wu <phillipwu9@gmail.com>
2022-03-08 22:03:49 +1300

    adding student signup alongside teacher

--- mission-3/src/pages/LogInSignUp.js
+++ mission-3/src/pages/LogInSignUp.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SignUpPageStudent from "../components/SignUpPageStudent";
+import SignUpPageTeacher from "../components/SignUpPageTeacher";
 import StudentLoginImage from "../images/Home/Students@2x.png";
 import TeacherLoginImage from "../images/Home/teacher@2x.png";
 
-function LoginSignUp() {
-  return <RegistrationForm />;
+function App() {
+  return <SignUpPageStudent />;
 }
 
 export default LoginSignUp;
