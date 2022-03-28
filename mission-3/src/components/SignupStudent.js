7590501fff8d3b625ea9aef57e7792c5ab9b3855
Phillip Wu <phillipwu9@gmail.com>
2022-03-28 22:21:55 +1300

    debugging

--- mission-3/src/components/SignupStudent.js
+++ mission-3/src/components/SignupStudent.js
@@ -9,25 +9,25 @@ function SignupStudent() {
           type="text"
           name="FUll Name"
           placeholder="Full Name"
-          className="Signup_inputs"
+          className="signup_inputs"
         />
         <input
           type="text"
           name="Email Address"
           placeholder="Email Address"
-          className="Signup_inputs"
+          className="signup_inputs"
         />
         <input
           type="text"
           name="Password"
           placeholder="Password"
-          className="Signup_inputs"
+          className="signup_inputs"
         />
         <input
           type="text"
           name="Confirm Password"
           placeholder="Confirm Password"
-          className="Signup_inputs"
+          className="signup_inputs"
         />
         <div>
           <button type="submit" className="submit_btn">
