4d8c4441079b65f5adc0e30d7a8f6e4d3dc44e95
MHS Technologies <mhstechcompany@gmail.com>
2022-03-31 00:13:53 +1300

    First Version of Profile

--- mission-3/src/pages/Profile.js
+++ mission-3/src/pages/Profile.js
@@ -1,7 +1,23 @@
+import "./Profile.css"
+import HomeHeader from "../components/HomeHeader"
+
 export default function Profile() {
-    return (
-        <div>
-            This is the Profile page
+  return (
+    <>
+      <HomeHeader />
+      <div className="profile_body">
+        <div className="profile_settings">
+          <h1> Photo </h1>
         </div>
-    )
+        <div className="profile_data">
+          <div>
+            <h1> Data </h1>
+          </div>
+          <div>
+            <button>BACK TO PROJECTS</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
