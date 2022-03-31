7741bb569317e074beb9a651aab0a8b45405bc4b
Phillip Wu <phillipwu9@gmail.com>
2022-03-31 19:12:01 +1300

    created .env and started routing

--- mission-3/src/index.js
+++ mission-3/src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+const express = require("express");
+const dotenv = require('dotenv').config();
+const app = express();
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+});
+
+
+app.post("./);
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+const PORT = process.env.EXPRESS_PORT;
+console.log('Server running at port', PORT);
+app.listen(PORT);
