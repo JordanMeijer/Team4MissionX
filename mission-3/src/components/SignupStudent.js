2a48dd48c5c75bb7e868c475f0214c3bd82086fe
MHS Technologies <mhstechcompany@gmail.com>
2022-04-04 20:28:18 +1200

    It has signing up student capability

--- mission-3/src/components/SignupStudent.js
+++ mission-3/src/components/SignupStudent.js
@@ -2,39 +2,67 @@ import React from "react";
 import "../components/LoginPage.css";
 
 function SignupStudent() {
+
+  function signUpAPI() {
+    const name = document.getElementById("full_name").value;
+    const email = document.getElementById("email_address").value;
+    const password= document.getElementById("password").value;
+    const confirm_password = document.getElementById("confirm_password").value;
+    const body = {
+      name: name,
+      email: email,
+      password: password
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    };
+
+    fetch("http://localhost:4000/create_user_student", requestOptions)
+      .then((response) => response.text())
+      .then((result) => alert(`You've successfully added a new user`))
+      .catch((error) => console.log("error", error));
+  }
+
   return (
     <div>
-      <form action="/user_create" method="POST">
-        <input
-          type="text"
-          name="create_full_name"
-          placeholder="Full Name"
-          className="signup_inputs"
-        />
-        <input
-          type="text"
-          name="create_email_address"
-          placeholder="Email Address"
-          className="signup_inputs"
-        />
-        <input
-          type="text"
-          name="create_password"
-          placeholder="Password"
-          className="signup_inputs"
-        />
-        <input
-          type="text"
-          name="confirm_password"
-          placeholder="Confirm Password"
-          className="signup_inputs"
-        />
-        <div>
-          <button type="submit" className="submit_btn">
-            SIGN UP
-          </button>
-        </div>
-      </form>
+      <input
+        type="text"
+        name="create_full_name"
+        placeholder="Full Name"
+        className="signup_inputs"
+        id="full_name"
+      />
+      <input
+        type="text"
+        name="create_email_address"
+        placeholder="Email Address"
+        className="signup_inputs"
+        id="email_address"
+      />
+      <input
+        type="text"
+        name="create_password"
+        placeholder="Password"
+        className="signup_inputs"
+        id="password"
+      />
+      <input
+        type="text"
+        name="confirm_password"
+        placeholder="Confirm Password"
+        className="signup_inputs"
+        id="confirm_password"
+      />
+      <div>
+        <button onClick={signUpAPI} className="submit_btn">
+          SIGN UP
+        </button>
+      </div>
     </div>
   );
 }
