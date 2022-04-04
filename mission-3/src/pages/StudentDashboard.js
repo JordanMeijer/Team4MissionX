53ab0daa59c65df3e1a4cb2e5437d5a198d3f095
MHS Technologies <mhstechcompany@gmail.com>
2022-04-04 19:22:25 +1200

    It is a first indicative of the result

--- mission-3/src/pages/StudentDashboard.js
+++ mission-3/src/pages/StudentDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./TeacherDashboard.css";
+import "./StudentDashboard.css";
 
 import SideBar from "../components/SideBar";
 import StudentTeacherHeader from "../components/StudentTeacherHeader";
@@ -12,14 +13,37 @@ export default function StudentDashboard() {
         text_middle="Ask Teacher for Help"
       />
       <div className="Dashboard">
-        <SideBar page="/studentdashboard" TeacherVersion={false}/>
+        <SideBar page="/studentdashboard" TeacherVersion={false} />
         <div className="DashboardContentsParentContainer">
           <div className="DashboardContentsChildContainer">
-            <h1 className="BeginnerCourseText">Explore Scratch Blocks</h1>
-            <p>
+            <h3 className="StudentDashboardH3Tag">Explore Scratch Blocks</h3>
+            <p className="StudentDashboardPTag">
               Learn the basic function of some basic scratch blocks such as
               “say,” “wait,” “go to” and “hide.”
             </p>
+            <div className="StudentDashboardBlocks">
+              <div>
+                <p className="StudentDashboardBlockPTag">Look Blocks</p>
+                <img 
+                  className="" 
+                  src="/images/Student Areas/Screen Shot 2020-04-29 at 9.09.52 PM@2x.png" 
+                  alt="control_blocks" />
+              </div>
+              <div>
+                <p className="StudentDashboardBlockPTag">Control Blocks</p>
+                <img 
+                  className="" 
+                  src="/images/Student Areas/Screen Shot 2020-04-29 at 9.09.52 PM@2x.png" 
+                  alt="control_blocks" />
+              </div>
+              <div>
+                <p className="StudentDashboardBlockPTag">Motion Blocks</p>
+                <img 
+                  className="" 
+                  src="/images/Student Areas/Screen Shot 2020-04-29 at 9.09.52 PM@2x.png" 
+                  alt="control_blocks" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
