70a271876605be867a4168e2a950a1ed7f554226
Phillip Wu <phillipwu9@gmail.com>
2022-04-04 13:27:16 +1200

    adding what i have so far

--- mission-4/server.js
+++ mission-4/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors')
-const dotenv = require('dotenv').config();
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const bodyParser = require(`body-parser`);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const connection = mysql.createConnection({
   host: process.env.HOST,
@@ -15,22 +17,45 @@ const connection = mysql.createConnection({
   password: process.env.PASSWORD,
 });
 
-app.get('/', (req, res) => {
-  console.log('Received a GET request to /');
+app.get("/", (req, res) => {
+  console.log("Received a GET request to /");
   // Run the SQL query, when you get a request to /
-  connection.query(`SELECT * FROM missio20_team4.Student LIMIT 15;`, (error, result) => {
-    if (error) {
-      console.log('Error', error);
-      res.send("You' got an error ! " + error.code);
-    } else {
-      console.log(result);
-      res.send(result);
+  connection.query(
+    `SELECT * FROM missio20_team4.Student LIMIT 15;`,
+    (error, result) => {
+      if (error) {
+        console.log("Error", error);
+        res.send("You' got an error ! " + error.code);
+      } else {
+        console.log(result);
+        res.send(result);
+      }
     }
-  });
+  );
 });
 
 //wasd
 
+app.post("/user_create", (req, res) => {
+  const name = req.body.create_full_name;
+  const email = req.body.create_email_address;
+  const password = req.body.create_password;
+
+  connection.query(
+    `INSERT INTO Student (name, email, password) VALUES (?, ?)`,
+    [name, email, password],
+    (error, result) => {
+      if (error) {
+        console.log(`Error`, error);
+        res.send("Invalid" + error.code);
+      } else {
+        console.log(result);
+        res.send("Sign up successful");
+      }
+    }
+  );
+});
+
 const PORT = process.env.PORT;
-console.log("server running at port", PORT)
+console.log("server running at port", PORT);
 app.listen(PORT);
