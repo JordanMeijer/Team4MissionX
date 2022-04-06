fa4de2ad020ed9d3eab06f2656b05e9639b053f8
MHS Technologies <mhstechcompany@gmail.com>
2022-04-07 00:25:55 +1200

    It adds some safeguards before calling the signup API

--- mission-3/src/components/LoginStudent.js
+++ mission-3/src/components/LoginStudent.js
@@ -2,27 +2,56 @@ import React from "react";
 import "../components/LoginPage.css";
 
 function LoginStudent() {
+
+function logInAPI() {
+  const email = document.getElementById("email_address").value;
+  const password = document.getElementById("password").value;
+  
+  if (email === '') {
+    alert(`Email address must be provided`)
+    return;
+  }
+
+  if (password === '') {
+    alert(`Please type your password`)
+    return;
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    //body: JSON.stringify(body),
+  };
+
+  fetch("http://localhost:4000/create_user_teacher", requestOptions)
+    .then((response) => response.text())
+    .then((result) => alert(`You've successfully added a new user`))
+    .catch((error) => console.log("error", error));
+}
+
   return (
     <div>
-      <form>
-        <input
-          type="text"
-          name="Email Address"
-          placeholder="Email Address"
-          className="login_inputs"
-        />
-        <input
-          type="text"
-          name="Password"
-          placeholder="Password"
-          className="login_inputs"
-        />
-        <div>
-          <button type="submit" className="submit_btn">
-            LOG IN
-          </button>
-        </div>
-      </form>
+      <input
+        type="text"
+        name="Email Address"
+        placeholder="Email Address"
+        className="login_inputs"
+        id="email_address"
+      />
+      <input
+        type="text"
+        name="Password"
+        placeholder="Password"
+        className="login_inputs"
+        id="password"
+      />
+      <div>
+        <button onClick={logInAPI} className="submit_btn">
+          LOG IN
+        </button>
+      </div>
     </div>
   );
 }
